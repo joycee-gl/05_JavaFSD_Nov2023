5b0b6c77e25eb7d66fc712068948efdc7f778b6e
joycee-gl <joycee.itwork.gl@gmail.com>
2023-10-14 18:11:56 +0530

    files

---
+++ 005_PreWork_Week_03/UserInputs/src/ScannerDemo1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ScannerDemo1 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new 
+			Scanner(System.in);
+		
+		System.out.println("Enter some text:");
+		
+		String text = scanner.nextLine();
+		
+		System.out.println("You have entered->" + text);
+		
+		if(scanner != null) {
+			scanner.close();
+		}
+	}
+}
---
+++ 005_PreWork_Week_03/UserInputs/src/ScannerReadErrorDemoV2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ScannerReadErrorDemoV2 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new 
+				Scanner(System.in);
+			
+		System.out.println("Enter some number:");
+		
+		boolean isAnIntegerValue = scanner.hasNextInt();
+		
+		if (isAnIntegerValue) {
+
+			int number = scanner.nextInt();			
+			System.out.println("You have entered->" + number);					
+		}else {
+			
+			System.out.println("You are expected to enter an integer number.");
+		}
+		
+		if(scanner != null) {
+			scanner.close();
+		}
+		
+	}
+}
