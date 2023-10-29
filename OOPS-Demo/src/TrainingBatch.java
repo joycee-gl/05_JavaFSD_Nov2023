d1c42c30ffcd5fca1fcebe91db4156cc9d114cef
joycee-gl <joycee.itwork.gl@gmail.com>
2023-10-29 18:24:13 +0530

    files

---
+++ OOPS-Demo/src/TrainingBatch.java
@@ -0,0 +1,12 @@
+
+public class TrainingBatch {
+	
+	public static void main(String[] args) {
+		
+		TrainingSession aug22Week2SessionObj = new TrainingSession();
+		
+		Learner ashaObj = new Learner();
+		Learner prabhuObj = new Learner();
+		
+	}
+}
---
+++ Package-Demo/src/networking/SocketConnection.java
@@ -0,0 +1,15 @@
+package networking;
+
+import networking.protocols.HttpProtocol;
+
+//import networking.protocols.*;
+
+public class SocketConnection {
+
+		public static void main(String[] args) {
+			
+			HttpProtocol httpProtocol = new HttpProtocol();
+			
+			
+		}
+}
