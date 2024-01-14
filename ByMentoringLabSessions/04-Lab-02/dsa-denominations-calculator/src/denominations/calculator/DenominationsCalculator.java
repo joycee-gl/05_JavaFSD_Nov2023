4b0da88b1df2cc557c8e7545332d0ada3973c9b3
joycee-gl <joycee.itwork.gl@gmail.com>
2024-01-14 17:29:53 +0530

    files

---
+++ ByMentoringLabSessions/04-Lab-02/dsa-denominations-calculator/src/denominations/calculator/DenominationsCalculator.java
@@ -0,0 +1,78 @@
+package denominations.calculator;
+
+public class DenominationsCalculator {
+	
+	private Integer[] denominations;
+	private int paymentAmount;	
+	
+	//constructor for denominations list and payment amount
+	
+	public DenominationsCalculator (Integer[] denominations, int paymentAmount) {
+		
+		this.denominations = denominations;
+		this.paymentAmount = paymentAmount;
+	}
+	
+	
+	private void performSort() {
+		//merge sort
+		
+	}
+	
+	//core method
+	
+	public void calculate() {
+		
+		performSort();
+		
+		int denominationIndex = 0;
+		
+		int workingPaymentAmount = paymentAmount;
+		
+		//total
+		int total = 0;
+		
+		while (denominationIndex < denominations.length) {
+			
+			int denomination = denominations[denominationIndex];
+			int noOfDenominations = workingPaymentAmount / denomination;
+			
+			//balance amount
+			int balanceAmount = workingPaymentAmount % denomination;
+			
+			//if denomination can be used or not
+			if(noOfDenominations != 0) {
+				
+				
+				//print the denomination value and its count
+				System.out.printf("Denomination value %d , NoOfTimes %d \r\n", 
+						denomination, noOfDenominations);
+				
+				System.out.println("Previous total " +total);
+				//update total with value from this iteration
+				
+				total = total + (denomination * noOfDenominations);
+				
+				System.out.println("Current total " + total);
+				
+			}
+			
+			//set pending balance for next iteration
+			workingPaymentAmount = balanceAmount;
+			
+			//when no more balance remaining, break from loop
+			if(workingPaymentAmount == 0) {
+				break;
+			}
+			
+			denominationIndex++;
+		}
+		
+		if(workingPaymentAmount != 0) {
+			
+			System.out.println("Unable to provide the exact denominations");
+		}
+		
+	}
+ 
+}
---
+++ ByMentoringLabSessions/04-Lab-02/dsa-denominations-calculator/src/denominations/calculator/DenominationsCalculatorTest.java
@@ -0,0 +1,47 @@
+package denominations.calculator;
+
+import java.util.Arrays;
+
+public class DenominationsCalculatorTest {
+	
+	public static void main(String[] args) {
+		
+//		test1();
+		test2();
+	}
+	
+	static void test1() {
+		
+//		Integer[] denominations = {5, 1, 10};
+		Integer[] denominations = {10, 5, 1};
+		int paymentAmount = 12;
+		
+		System.out.printf("For denominations %s and for payment amount %d", 
+				Arrays.toString(denominations), paymentAmount);
+		System.out.println();
+		
+		DenominationsCalculator calculator = 
+				new DenominationsCalculator(denominations, paymentAmount);
+		
+		calculator.calculate();
+		
+	}
+	
+	static void test2() {
+		
+//		Integer[] denominations = {12, 5, 123, 18};
+		Integer[] denominations = {123, 18, 12, 5};
+		int paymentAmount = 160;
+		
+		System.out.printf("For denominations %s and for payment amount %d", 
+				Arrays.toString(denominations), paymentAmount);
+		System.out.println();
+		
+		DenominationsCalculator calculator = 
+				new DenominationsCalculator(denominations, paymentAmount);
+		
+		calculator.calculate();
+		
+	}
+
+}
